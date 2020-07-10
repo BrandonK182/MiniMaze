417b6a6cc94b703e24a67195ebac33a7997a21c1
BrandonK182 <67713233+BrandonK182@users.noreply.github.com>
2020-07-09 20:45:30 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,70 @@
 #include "mainwindow.h"
 
-#include <QApplication>
+#include <iostream>
 
-int main(int argc, char *argv[])
+using namespace std;
+
+int main()
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    char move;
+    bool win = false;
+    start();
+    while(win==false)
+    {
+        cin>>move;
+        if(move=='w' || move=='W')
+        {
+            moveUp();
+        }
+        if(move=='s' || move=='S')
+        {
+            movedown();
+        }
+        if(move=='a' || move=='A')
+        {
+            moveLeft();
+        }
+        if(move=='d' || move=='D')
+        {
+            moveRight();
+        }
+    return 0;
+}
+
+void start()
+{
+    player aPlayer;
+    savedPlayer = aPlayer;
+    cout<<savedPlayer;
+}
+
+void moveUp()
+{
+    savedPlayer.movePlayerUp();
+    cout<<savedPlayer;
+}
+
+void moveRight()
+{
+    savedPlayer.movePlayerRight();
+    cout<<savedPlayer;
+}
+
+void moveDown()
+{
+    savedPlayer.movePlayerDown();
+    if(savedPlayer.getPlayerRow()==savedPlayer.totalRows)
+    {
+        cout<<"you win!";
+    }
+    else
+    {
+        cout<<savedPlayer;
+    }
+}
+
+void moveLeft()
+{
+    savedPlayer.movePlayerLeft();
+    cout<<savedPlayer;
 }
